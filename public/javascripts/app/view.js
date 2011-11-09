a1512f978f44b403bec09648d9df6017946c7a09
Dave Winders <david.winders@gmail.com>
2011-11-09 18:49:36 +0000

    Update public/javascripts/app/view.js

--- public/javascripts/app/view.js
+++ public/javascripts/app/view.js
@@ -1,33 +1,9 @@
 App.View = (function(lng, app, undefined) {
     lng.View.Template.create('twitterfriends','<li id="{{id}}"><strong>@{{screen_name}}</strong></li>')
 
-	 var todo = function(id) {
-        lng.Data.Sql.select('todo', {id:id}, function(result){
-            if (result.length > 0) {
-                var data = result[0];
-                lng.Data.Cache.set('current_todo', data);
-                $('#txtEditName').val(data.name);
-                $('#txtEditDescription').val(data.description);
-                $('#txtEditName').val(data.name);
-
-                lng.Router.section('view');
-            }
-        });
-	};
-
-	var returnToMain = function(message, icon) {
-		lng.Sugar.Growl.show(message, icon, true);
-	//	App.Data.refresh();
-
-		setTimeout(function() {
-			lng.Router.back();
-			lng.Sugar.Growl.hide();
-		}, 500);
-	};
 
 	return{
-		todo: todo,
-		returnToMain: returnToMain
+	
 	}
 
 })(LUNGO, App);
